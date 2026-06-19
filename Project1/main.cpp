72a453f5854bb970798a17cac5075439b2857132
linian <2836409870@qq.com>
2026-06-19 19:26:38 +0800

    添加项目文件。

---
+++ Project1/main.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+struct Task {
+    string title;
+    bool done;
+};
+
+void printTask(const vector<Task>& tasks) {
+    cout << "My Task List" << endl;
+    if (tasks.empty()) {
+        cout << "No tasks" << endl;
+    }
+    else {
+        for (size_t i = 0; i < tasks.size(); i++) {
+            cout << i + 1 << ". " ;
+            if (tasks[i].done){
+                cout << "[x] ";
+            }
+            else {
+                cout<<"[ ] ";
+            }
+            cout << tasks[i].title << endl;
+        }
+    }
+}
+
+int main()
+{
+    vector<Task>tasks;
+    int choice;
+
+    while(true) {
+        cout << "Study Todo" << endl;
+        cout << "1. Add Task" << endl;
+        cout << "2. List tasks" << endl;
+        cout << "3. Mark task as done" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+        cin.ignore();
+        
+        if (choice == 1) {
+            string newTask;
+            cout << "Please enter a task (enter 'quit' to exit):" << endl;
+
+            while (true) {
+                cout << ">";
+                getline(cin, newTask);
+
+                if (newTask == "quit")
+                    break;
+                if (newTask.empty()) {
+                    cout << "Task cannot be empty, please re-enter" << endl;
+                    continue;
+                }
+
+                Task task;
+                task.title = newTask;
+                task.done = false;
+
+                tasks.push_back(task);
+
+                cout << "Task added: " << newTask << endl;
+                cout << "Total " << tasks.size() << " task(s) in the list" << endl;
+            }
+        }
+        else if (choice == 2)
+            printTask(tasks);
+
+        else if (choice == 3) {
+            int tasknum;
+            cout << "Which task did you finish? Enter number: ";
+            cin >> tasknum;
+            cin.ignore();
+
+            if (tasknum >= 1 && tasknum <= static_cast<int>(tasks.size())) {
+                tasks[tasknum - 1].done = true;
+                cout << "Task marked as done." << endl;
+            }
+            else {
+                cout << "Invalid task number." << endl;
+            }
+        }
+        else if (choice == 4) {
+            break;
+        }
+
+        else{
+            cout << "Invalid choice!" << endl;
+        }
+    }
+
+    return 0;
+}
